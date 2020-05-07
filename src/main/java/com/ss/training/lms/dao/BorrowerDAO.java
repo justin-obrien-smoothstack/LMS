67231aacc12b91728b499a62427b22a718310b38
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-07 13:56:31 -0600

    Apply STS auto-formatting in 4 DAO classes
    BookDAO, BookGenreDAO, BookLoanDAO, BorrowerDAO

--- src/main/java/com/ss/training/lms/dao/BorrowerDAO.java
+++ src/main/java/com/ss/training/lms/dao/BorrowerDAO.java
@@ -13,38 +13,40 @@ import com.ss.training.lms.entity.Borrower;
 @Component
 public class BorrowerDAO extends BaseDAO<Borrower> {
 
-    public Integer addBorrower(Borrower borrower, Connection conn) throws ClassNotFoundException, SQLException {
-        return saveWithPK("INSERT INTO tbl_borrower (name, address, phone) VALUES (?, ?, ?)", new Object[] { borrower.getName(), borrower.getAddress(), borrower.getPhone()}, conn);
-    }
+	public Integer addBorrower(Borrower borrower, Connection conn) throws ClassNotFoundException, SQLException {
+		return saveWithPK("INSERT INTO tbl_borrower (name, address, phone) VALUES (?, ?, ?)",
+				new Object[] { borrower.getName(), borrower.getAddress(), borrower.getPhone() }, conn);
+	}
 
-    public void updateBorrower(Borrower borrower, Connection conn) throws ClassNotFoundException, SQLException {
-        save("UPDATE tbl_borrower SET name = ?, address = ?, phone = ? WHERE cardNo = ?",
-                new Object[] { borrower.getName(), borrower.getAddress(), borrower.getPhone(), borrower.getCardNo()}, conn);
-    }
+	public void updateBorrower(Borrower borrower, Connection conn) throws ClassNotFoundException, SQLException {
+		save("UPDATE tbl_borrower SET name = ?, address = ?, phone = ? WHERE cardNo = ?",
+				new Object[] { borrower.getName(), borrower.getAddress(), borrower.getPhone(), borrower.getCardNo() },
+				conn);
+	}
 
-    public void deleteBorrower(Borrower borrower, Connection conn) throws ClassNotFoundException, SQLException {
-        save("DELETE FROM tbl_borrower WHERE cardNo = ?", new Object[] { borrower.getCardNo() }, conn);
-    }
+	public void deleteBorrower(Borrower borrower, Connection conn) throws ClassNotFoundException, SQLException {
+		save("DELETE FROM tbl_borrower WHERE cardNo = ?", new Object[] { borrower.getCardNo() }, conn);
+	}
 
-    public List<Borrower> readAllBorrowers(Connection conn) throws ClassNotFoundException, SQLException {
-        return read("SELECT * FROM tbl_borrower", null, conn);
-    }
+	public List<Borrower> readAllBorrowers(Connection conn) throws ClassNotFoundException, SQLException {
+		return read("SELECT * FROM tbl_borrower", null, conn);
+	}
 
-    public List<Borrower> readABorrower(Integer cardNo, Connection conn) throws SQLException {
-        return read("SELECT * FROM tbl_borrower WHERE cardNo = ?;", new Object[] { cardNo }, conn);
-    }
+	public List<Borrower> readABorrower(Integer cardNo, Connection conn) throws SQLException {
+		return read("SELECT * FROM tbl_borrower WHERE cardNo = ?;", new Object[] { cardNo }, conn);
+	}
 
-    @Override
-    public List<Borrower> extractData(ResultSet rs) throws SQLException {
-        List<Borrower> borrowers = new ArrayList<>();
-        while (rs.next()) {
-            Borrower borrower = new Borrower();
-            borrower.setCardNo(rs.getInt("cardNo"));
-            borrower.setName(rs.getString("name"));
-            borrower.setAddress(rs.getString("name"));
-            borrower.setPhone(rs.getString("name"));
-            borrowers.add(borrower);
-        }
-        return borrowers;
-    }
+	@Override
+	public List<Borrower> extractData(ResultSet rs) throws SQLException {
+		List<Borrower> borrowers = new ArrayList<>();
+		while (rs.next()) {
+			Borrower borrower = new Borrower();
+			borrower.setCardNo(rs.getInt("cardNo"));
+			borrower.setName(rs.getString("name"));
+			borrower.setAddress(rs.getString("name"));
+			borrower.setPhone(rs.getString("name"));
+			borrowers.add(borrower);
+		}
+		return borrowers;
+	}
 }

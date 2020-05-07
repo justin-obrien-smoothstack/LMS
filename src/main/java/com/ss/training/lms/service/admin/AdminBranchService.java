9cc2729ce726d8badb8312f21388e0e150a307ea
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-07 15:18:23 -0600

    Apply STS auto-formatting to 3 service classes
    AdminAuthorService, AdminBranchService, AdminPublisherService

--- src/main/java/com/ss/training/lms/service/admin/AdminBranchService.java
+++ src/main/java/com/ss/training/lms/service/admin/AdminBranchService.java
@@ -11,105 +11,105 @@ import com.ss.training.lms.dao.LibraryBranchDAO;
 import com.ss.training.lms.entity.LibraryBranch;
 
 public class AdminBranchService {
-    ConnectionUtil connUtil;
-    
-    public Integer addABranch(LibraryBranch branch) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
-            Integer primaryKey = branchDAO.addBranch(branch);
-            conn.commit();
-            return primaryKey;
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not add that branch.");
-            e.printStackTrace();
-            conn.rollback();
-            return 0;
-        } finally {
-			if(conn!=null){
-				conn.close();
-			}
-		}
-    }
+	ConnectionUtil connUtil;
 
-    public void deleteABranch(LibraryBranch branch) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
-            BookLoanDAO loanDAO = new BookLoanDAO(conn);
-            loanDAO.deleteBookLoansByBranch(branch.getBranchId());
-            BookCopiesDAO copiesDAO = new BookCopiesDAO(conn);
-            copiesDAO.deleteBookLoansByBranch(branch.getBranchId());
-            branchDAO.deleteBranch(branch);
-            conn.commit();
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not delete that branch.");
-            conn.rollback();
-        } finally {
-			if(conn!=null){
+	public Integer addABranch(LibraryBranch branch) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
+			Integer primaryKey = branchDAO.addBranch(branch);
+			conn.commit();
+			return primaryKey;
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not add that branch.");
+			e.printStackTrace();
+			conn.rollback();
+			return 0;
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
 
-    public void updateABranch(LibraryBranch branch) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
-            branchDAO.updateBranch(branch);
-            conn.commit();
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not update that branch.");
-            conn.rollback();
-        } finally {
-			if(conn!=null){
+	public void deleteABranch(LibraryBranch branch) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
+			BookLoanDAO loanDAO = new BookLoanDAO(conn);
+			loanDAO.deleteBookLoansByBranch(branch.getBranchId());
+			BookCopiesDAO copiesDAO = new BookCopiesDAO(conn);
+			copiesDAO.deleteBookLoansByBranch(branch.getBranchId());
+			branchDAO.deleteBranch(branch);
+			conn.commit();
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not delete that branch.");
+			conn.rollback();
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
 
-    public LibraryBranch readABranch(Integer branchId) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
-            List<LibraryBranch> branches = branchDAO.readABranch(branchId);
-            if(branches.size() == 0) {
-                return null;
-            }
-            return branches.get(0);
-        } catch ( SQLException e) {
-            System.out.println("We could not read the branch.");
-            conn.rollback();
-            return null;
-        } finally {
-			if(conn!=null){
+	public void updateABranch(LibraryBranch branch) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
+			branchDAO.updateBranch(branch);
+			conn.commit();
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not update that branch.");
+			conn.rollback();
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
 
-    public List<LibraryBranch> readAllBranches() throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
-            List<LibraryBranch> branches = branchDAO.readAllBranches();
-            if(branches.size() == 0) {
-                return null;
-            }
-            return branches;
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not read the branch.");
-            conn.rollback();
-            return null;
-        } finally {
-			if(conn!=null){
+	public LibraryBranch readABranch(Integer branchId) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
+			List<LibraryBranch> branches = branchDAO.readABranch(branchId);
+			if (branches.size() == 0) {
+				return null;
+			}
+			return branches.get(0);
+		} catch (SQLException e) {
+			System.out.println("We could not read the branch.");
+			conn.rollback();
+			return null;
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
+
+	public List<LibraryBranch> readAllBranches() throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			LibraryBranchDAO branchDAO = new LibraryBranchDAO(conn);
+			List<LibraryBranch> branches = branchDAO.readAllBranches();
+			if (branches.size() == 0) {
+				return null;
+			}
+			return branches;
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not read the branch.");
+			conn.rollback();
+			return null;
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
 }
--- src/main/java/com/ss/training/lms/service/admin/AdminPublisherService.java
+++ src/main/java/com/ss/training/lms/service/admin/AdminPublisherService.java
@@ -11,102 +11,102 @@ import com.ss.training.lms.entity.Publisher;
 
 public class AdminPublisherService {
 	ConnectionUtil connUtil;
-    
-    public Integer addAPublisher(Publisher publisher) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            PublisherDAO borDAO = new PublisherDAO(conn);
-            Integer primaryKey = borDAO.addPublisher(publisher);
-            conn.commit();
-            return primaryKey;
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not add that publisher.");
-            e.printStackTrace();
-            conn.rollback();
-            return 0;
-        } finally {
-			if(conn!=null){
-				conn.close();
-			}
-		}
-    }
 
-    public void deleteAPublisher(Publisher publisher) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            PublisherDAO pubDAO = new PublisherDAO(conn);
-            BookDAO bookDAO = new BookDAO(conn);
-            bookDAO.deleteBooksByPublisher(publisher.getPublisherID());
-            pubDAO.deletePublisher(publisher);
-            conn.commit();
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not delete that publisher.");
-            conn.rollback();
-        } finally {
-			if(conn!=null){
+	public Integer addAPublisher(Publisher publisher) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			PublisherDAO borDAO = new PublisherDAO(conn);
+			Integer primaryKey = borDAO.addPublisher(publisher);
+			conn.commit();
+			return primaryKey;
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not add that publisher.");
+			e.printStackTrace();
+			conn.rollback();
+			return 0;
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
 
-    public void updateAPublisher(Publisher publisher) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            PublisherDAO borDAO = new PublisherDAO(conn);
-            borDAO.updatePublisher(publisher);
-            conn.commit();
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not update that publisher.");
-            conn.rollback();
-        } finally {
-			if(conn!=null){
+	public void deleteAPublisher(Publisher publisher) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			PublisherDAO pubDAO = new PublisherDAO(conn);
+			BookDAO bookDAO = new BookDAO(conn);
+			bookDAO.deleteBooksByPublisher(publisher.getPublisherID());
+			pubDAO.deletePublisher(publisher);
+			conn.commit();
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not delete that publisher.");
+			conn.rollback();
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
 
-    public Publisher readAPublisher(Integer pubId) throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            PublisherDAO borDAO = new PublisherDAO(conn);
-            List<Publisher> publishers = borDAO.readAPublisher(pubId);
-            if(publishers.size() == 0) {
-                return null;
-            }
-            return publishers.get(0);
-        } catch ( SQLException e) {
-            System.out.println("We could not read the publisher.");
-            conn.rollback();
-            return null;
-        } finally {
-			if(conn!=null){
+	public void updateAPublisher(Publisher publisher) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			PublisherDAO borDAO = new PublisherDAO(conn);
+			borDAO.updatePublisher(publisher);
+			conn.commit();
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not update that publisher.");
+			conn.rollback();
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
 
-    public List<Publisher> readAllPublishers() throws SQLException {
-        Connection conn = null;
-        try {
-            conn = connUtil.getConnection();
-            PublisherDAO borDAO = new PublisherDAO(conn);
-            List<Publisher> publishers = borDAO.readAllPublishers();
-            if(publishers.size() == 0) {
-                return null;
-            }
-            return publishers;
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("We could not read the publishers.");
-            conn.rollback();
-            return null;
-        } finally {
-			if(conn!=null){
+	public Publisher readAPublisher(Integer pubId) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			PublisherDAO borDAO = new PublisherDAO(conn);
+			List<Publisher> publishers = borDAO.readAPublisher(pubId);
+			if (publishers.size() == 0) {
+				return null;
+			}
+			return publishers.get(0);
+		} catch (SQLException e) {
+			System.out.println("We could not read the publisher.");
+			conn.rollback();
+			return null;
+		} finally {
+			if (conn != null) {
 				conn.close();
 			}
 		}
-    }
+	}
+
+	public List<Publisher> readAllPublishers() throws SQLException {
+		Connection conn = null;
+		try {
+			conn = connUtil.getConnection();
+			PublisherDAO borDAO = new PublisherDAO(conn);
+			List<Publisher> publishers = borDAO.readAllPublishers();
+			if (publishers.size() == 0) {
+				return null;
+			}
+			return publishers;
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("We could not read the publishers.");
+			conn.rollback();
+			return null;
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
 }
